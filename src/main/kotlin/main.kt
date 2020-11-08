789a23f6db190b807b208090ce644283e8f5b38d
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-08 13:30:55 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,9 @@
 fun main() {
     var name:String? = "Przemysław"
     var surname:String = "Stokłosa"
-    name = null
+
+    //name = null
+
+    println("Długość napisu $name: ${name?.length}")
+    println("Długość napisu $surname: ${surname.length}")
 }
