704edb4d5585038e1f2dabe13f006bb8bd25b7ea
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-11-08 13:36:20 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,20 @@
+import java.lang.NullPointerException
+
 fun main() {
     var name:String? = "Przemysław"
     var surname:String = "Stokłosa"
 
-    //name = null
+    if(true)
+        name = null
 
     println("Długość napisu $name: ${name?.length}")
+
+    try {
+        println("Długość napisu $name: ${name!!.length}")
+    }catch(e: NullPointerException){
+        println("Przechwycono wyjątek: ")
+        println(e.stackTraceToString())
+    }
+
     println("Długość napisu $surname: ${surname.length}")
 }
